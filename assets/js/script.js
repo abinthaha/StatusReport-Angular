dba6f156409f25b6a2542540fe242a7e714e5991
Abin Thaha <abin@qburst.com>
2015-09-18 14:43:22 +0530

    Folder structure changed

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+    // Creating the module.
+        //Including ngRoute for all our routing needs.
+var spaceApp = angular.module('spaceApp', ['ngRoute']);
+
+    //Configuring our routes
+    spaceApp.config(function($routeProvider) {
+    		$routeProvider
+    			// route for the home page
+    			.when('/', {
+    				templateUrl : 'app/views/home.html'
+    			})
+
+    			// route for the about page
+    			.when('/status', {
+    				templateUrl : 'app/views/dailyStatus.html',
+                    controller : 'statusController'
+    			});
+    	});
+
+    // Creating the controller
+spaceApp.controller('mainController', function($scope){
+    $scope.message = 'Hello WOrld';
+});
+
+spaceApp.controller('statusController', function($scope) {
+
+    status = this;
+    $scope.message = 4;
+
+    $.getJSON("app/items.json", function(data){
+        $scope.historyItems = data.builtItems;
+        $scope.projects = data.projects;
+        $scope.types = data.types;
+        console.log(data);
+    });
+
+    $scope.addItem =  function() {
+        $scope.historyItems.push(
+            {
+                date: $('#dateWork').val(),
+                pro_name: $scope.getPro,
+                act_type: $scope.getType,
+                time: $scope.gethours +':' +$scope.getMints ,
+                description: $scope.getDescription
+            }
+        );
+        $('#ItemForm')[0].reset();
+    };
+});
---
+++ assets/js/status.js
@@ -0,0 +1,5 @@
+angular.module('StatusReport', [])
+.controller('statusReports', function(){
+
+    
+});
